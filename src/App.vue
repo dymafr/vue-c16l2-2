3f4e03da93f7cd1521dcef22d682a7b1d2676a60
dymafr <erwan1309@hotmail.com>
2022-03-29 12:02:02 +0200

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="p-20">
-    <button class="btn btn-primary" @click="show = !show">Display</button>
+    <button class="btn btn-primary" @click="show = !show">Cliquez !</button>
     <Transition @enter="onEnter" appear name="fade">
-      <h1 v-if="show">Bonjour !</h1>
+      <p v-if="show">
+        Principes fertur et exitiale aliquando oblato factitarunt quod
+        propositum quod illo ob illo numquam obstinatum obstinatum quoque
+        aetatis revocari iussisse vitium factitarunt inexorabiles aetatis quod
+        accendente fertur aetatis iussisse vitium similia iussisse factitarunt
+        propositum similia neminem revocari adulatorum accendente fertur ob
+        adulatorum principes accendente inexorabiles elogio vitium exitiale
+        aliis progressu eius eius eius et non quoque vel in in quod more de
+        propositum quod oblato factitarunt poenae hoc principes exitiale cohorte
+        hoc similia hoc aliis quod de in in addictum eius adulatorum quoque et
+        ob oblato neminem numquam adulatorum addictum ob quod cohorte aliis
+        neminem vel revocari hoc hoc similia.
+      </p>
     </Transition>
   </div>
 </template>
